c27b2b4e43b7327ca188fecc660c76d155ce8d23
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-08-20 16:15:59 +0500

    Update s.ts

--- s.ts
+++ s.ts
@@ -1,1 +1,40 @@
-console.log("Hello from s");
+namespace incorrect {
+  class Order {
+    public calculateTotalSum() {}
+    public addItem() {}
+    public deleteItem() {}
+    public getItemCount() {}
+  
+    public printOrder() {}
+    public showOrder() {}
+  
+    public load() {}
+    public save() {}
+    public update() {}
+    public delete() {}
+  }
+}
+
+namespace correct {
+  class Order {
+    public calculateTotalSum() {}
+    public addItem() {}
+    public deleteItem() {}
+    public getItemCount() {}
+  }
+
+  class OrderView {
+    public printOrder() {}
+    public showOrder() {}
+  }
+
+  class OrderDB {
+    public load() {}
+    public save() {}
+    public update() {}
+    public delete() {}
+  }
+}
+
+
+// The example is taken from: https://habr.com/ru/post/208442/ (rus)
