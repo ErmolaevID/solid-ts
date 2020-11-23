4bf68fcb77f970aaf5d4d8016b6dbcf7ed864c48
Ilya <Ermolaev.ID@yandex.ru>
2020-11-23 12:28:35 +0500

    Update 1_S

--- solid/1_S.ts
+++ solid/1_S.ts
@@ -1,40 +1,65 @@
 namespace incorrect {
-  class Order {
-    public calculateTotalSum() {}
-    public addItem() {}
-    public deleteItem() {}
-    public getItemCount() {}
-  
-    public printOrder() {}
-    public showOrder() {}
-  
-    public load() {}
-    public save() {}
-    public update() {}
-    public delete() {}
+  class Book {
+    private _title: string;
+    private _author: string;
+    private _currentPage: number;
+
+    public get title(): string { return this._title; }
+    public get author(): string { return this._author; }
+    public get currentPage(): number { return this._currentPage; }
+
+    constructor(title: string, author: string) {
+      this._title = title;
+      this._author = author;
+      this._currentPage = 1;
+    }
+
+    public turnPage() {
+      this._currentPage++;
+    }
+        
+    public printCurrentPage() {
+      return "page content";
+    }
   }
+  // If you need new html print or xml etc. You need to change Book class 
 }
 
 namespace correct {
-  class Order {
-    public calculateTotalSum() {}
-    public addItem() {}
-    public deleteItem() {}
-    public getItemCount() {}
+  class Book {
+    private _title: string;
+    private _author: string;
+    private _currentPage: number;
+
+    public get title(): string { return this._title; }
+    public get author(): string { return this._author; }
+    public get currentPage(): number { return this._currentPage; }
+
+
+    constructor(title: string, author: string) {
+      this._title = title;
+      this._author = author;
+      this._currentPage = 1;
+    }
+
+    public turnPage(): void {
+      this._currentPage++;
+    }
   }
 
-  class OrderView {
-    public printOrder() {}
-    public showOrder() {}
+  interface Printer {
+    print(content: string): string;
   }
-
-  class OrderDB {
-    public load() {}
-    public save() {}
-    public update() {}
-    public delete() {}
+  
+  class PlainTextPrinter implements Printer {
+    public print(content: string): string {
+      return content;
+    }
   }
+  class HtmlPrinter implements Printer {
+    public print(content: string): string {
+      return `<p>${content}</p>`;
+    }
+  }
+  //  If you need new printer you can create new class which implements Printer
 }
-
-
-// The example is taken from: https://habr.com/ru/post/208442/ (rus)
